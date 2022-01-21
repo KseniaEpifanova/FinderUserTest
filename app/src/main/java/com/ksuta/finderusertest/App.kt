5fa38cda2594c845aa88650d07b61370dc14adc9
EpifanovKA <epifanova-ksenia@rambler.ru>
2022-01-21 21:08:21 +0000

    Setting App

--- app/src/main/java/com/ksuta/finderusertest/App.kt
+++ app/src/main/java/com/ksuta/finderusertest/App.kt
@@ -2,11 +2,17 @@ package com.ksuta.finderusertest
 
 import androidx.lifecycle.LifecycleObserver
 import androidx.multidex.MultiDexApplication
+import com.ksuta.finderusertest.network.ComponentInjector
 
-class App: MultiDexApplication(), LifecycleObserver {
+class App : MultiDexApplication(), LifecycleObserver {
+
+    val componentInjector: ComponentInjector by lazy {
+        ComponentInjector(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
+        componentInjector.getAppComponent().inject(this)
     }
 
 }
